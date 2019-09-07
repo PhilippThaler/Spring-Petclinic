3655b02567e9129973cabc10be7aba0f44d5d5e6
Philipp Thaler <philipp@thaler.fyi>
2019-09-07 22:09:11 +0200

    refactor project

---
+++ petclinic-data/src/main/java/fyi/thaler/springpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package fyi.thaler.springpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ petclinic-data/src/main/java/fyi/thaler/springpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package fyi.thaler.springpetclinic.model;
+
+public class Vet extends Person {
+}
